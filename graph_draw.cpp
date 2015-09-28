0e280fb21e9f7c2ff224b9d9c1f284bd66a6080b
Josh Tobin <tobinrj@tcd.ie>
2015-09-27 19:15:48 -0700

    Fixes a bug in the force drawing algorithm
    When computing the force that one vertex exerts on another, the code
    uses the difference between the two vertices as the direction of the
    force.  Previously this was not normalised, leading to the vertices
    blowing up if sufficiently far apart.

--- graph_draw.cpp
+++ graph_draw.cpp
@@ -37,12 +37,11 @@ void force_draw(CDrawing& drawing, Graph& g, int x_size, int y_size)
   for(int i=0; i<n; i++)
   {
     forces[i] = new CPoint(0,0);
-    pts[i] = new CPoint(drand48() * (double) (x_size / 2.), drand48() * (double) (y_size / 2.));
+    pts[i] = new CPoint(drand48() * (double) (x_size / 1.), drand48() * (double) (y_size / 1.));
   }
 
   for(int i=0; i<n_iter; i++)
   {
-    //cout << "Iter " << i << endl;
     for(int j=0; j<n; j++)
     {
       double fx = 0.,  fy = 0., dist, fc;
@@ -52,26 +51,19 @@ void force_draw(CDrawing& drawing, Graph& g, int x_size, int y_size)
           continue;
         dist = sqrt(pow(pts[j]->x - pts[k]->x,2.) + pow(pts[j]->y - pts[k]->y,2.));
         if(g.isConnected(j,k))
-          fc = -c1 * log(dist / c2);
+          fc = - c1 * log(dist / c2);
         else
           fc = c3 / pow(dist,2.);
+        
+        fc *= c4;        
 
-        fc *= c4;
-
-        fx += fc * (pts[j]->x - pts[k]->x);
-        fy += fc * (pts[j]->y - pts[k]->y);
+        fx += fc * (pts[j]->x - pts[k]->x) / dist;
+        fy += fc * (pts[j]->y - pts[k]->y) / dist;
       }
 
       pts[j]->x += fx;
       pts[j]->y += fy;
     }
-
-    for(int j=0; j<n; j++)
-    {
-      //cout << "\t" << pts[j]->x << ' ' << pts[j]->y << endl;
-    }
-    
-
   }
   
   for(int i=0; i<n; i++)
